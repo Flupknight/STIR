87ffdeb876c3984b5fcd00ea39a48e5455f02191
Filip Nyhamn <filnyh09@gvgs.no>
2026-05-08 22:56:43 +0200

    Initialize pygame and set up the main display loop

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+import pygame
+
+pygame.display.init
+screen = pygame.display.set_mode((640, 480))
+
+runing = bool(True)
+
+
+while runing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            runing = False
