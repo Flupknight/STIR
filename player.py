cf79671a2e53c3efa6268ffe238d7b68d9ff306e
Filip Nyhamn <filnyh09@gvgs.no>
2026-05-11 12:15:54 +0200

    Refactor player class to initialize position variables and update movement logic

--- player.py
+++ player.py
@@ -7,6 +7,18 @@ class player(pygame.sprite.Sprite):
         self.image.fill((0,0,0))
         self.rect = self.image.get_rect()
         self.rect.center = (400,250)
+        self.xPos = 400
+        self.yPos = 250
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+    
+    def update(self):
+        # moving controls
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.xPos -= 1
+        if keys[pygame.K_d]:
+            self.xPos += 1
+        
+        self.rect.center = (self.xPos, self.yPos)
